48b5bd11bc93b84b9d9b059be49d59abcef3ca36
Frans Dressler <frans@Franss-MacBook-Air.local>
2023-09-03 16:35:29 +0200

    solved connection problem to database

--- controller/Register.js
+++ controller/Register.js
@@ -27,7 +27,8 @@ const handleRegister = (req, res,db,bcrypt) => {
             .catch(trx.rollback)
     })
     
-    .catch(err => res.status(400).json('unable to register:',err));
+    .catch(err => res.status(400).json({message: 'unable to register', error: err}))
+
 };
 
 module.exports = {
